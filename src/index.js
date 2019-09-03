749035189f9dd0e5da715f61e064e01785e2a04a
NKV-88 <sysadm86@gmail.com>
2019-09-03 16:30:37 +0500

    Разбираюсь с пройденным до ДЗ еще не добрался.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//import DisplayElement from "./app/components/DisplayElement";
-//import './app/styles/style.css'
-import Users from './app/components/Users';
 
-const USERS = ['Anna', 'Olga', 'Kot', 'Anton'];
 
-ReactDom.render ( <Users items={USERS}/>, document.getElementById('root'));
+ReactDom.render ( <div></div>, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //import DisplayElement from "./app/components/DisplayElement";
+// //import './app/styles/style.css'
+// import Users from './app/components/Users';
+
+// const USERS = ['Anna', 'Olga', 'Kot', 'Anton'];

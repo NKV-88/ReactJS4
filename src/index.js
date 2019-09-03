8f22d4a6e46f55d1ec2f89dd597f348f6c55dda7
NKV-88 <sysadm86@gmail.com>
2019-09-03 12:02:04 +0500

    настройка

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import '.app/styles/style.css';
+
+ReactDom.render (
+    <div>
+        1
+    </div>,
+    document.getElementById('root'));

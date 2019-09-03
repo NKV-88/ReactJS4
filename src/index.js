0541475cf7cce2d67cdb51e62475f96a353266ec
NKV-88 <sysadm86@gmail.com>
2019-09-03 16:25:52 +0500

    Первая половина урока

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Comp from "./app/components/DisplayElement";
-import './app/styles/style.css'
+//import DisplayElement from "./app/components/DisplayElement";
+//import './app/styles/style.css'
+import Users from './app/components/Users';
 
+const USERS = ['Anna', 'Olga', 'Kot', 'Anton'];
 
-ReactDom.render ( <Comp />, document.getElementById('root'));
+ReactDom.render ( <Users items={USERS}/>, document.getElementById('root'));
